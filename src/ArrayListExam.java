67f1e123c9b34d617758c33c49152d8fd67e2efc
Ji Hyun <nswwangjin@google.com>
2016-01-25 16:14:13 +0900

    연습문제답

--- src/ArrayListExam.java
+++ src/ArrayListExam.java
@@ -15,16 +15,138 @@ public class ArrayListExam {
         list.add(pic3);
         list.add(pic4);
         list.add(pic5);
-
 //        pic1.getMaxX() : 오른쪽 끝 x좌표
 //        pic1.getWidth() : 가로길이
+//        pic1.getHeight()
 //        pic1.translate(x ,y); 이동
 //        pic1.draw();
-//        for (int i = 0; i < list.size(); i++) {
-//            Picture picture = list.get(i);
-//            picture.draw();
+
+        1 번답
+
+
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < list.size(); i++) {
+
+            Picture picture = list.get(i);
+            k = picture.getWidth() + 30;
+
+            picture.translate(j, 0);
+
+
+            picture.draw();
+            j += k;
+
 
         }
 
     }
 }
+
+
+2번답
+
+        int j=0;
+
+        for(int i=0;i<list.size();i++){
+
+        Picture picture=list.get(i);
+        j+=picture.getWidth();
+
+
+        }
+        System.out.println(j);
+
+        }
+        }
+
+        3번답
+
+
+        int j=0;
+        int k=0;
+        for(int i=0;i<list.size();i++){
+
+        Picture picture=list.get(i);
+        k=picture.getWidth();
+
+        picture.translate(j,0);
+
+
+        if(picture.getHeight()>k){
+        picture.draw();
+        j+=k;
+
+        }
+
+        }
+
+
+        4번답
+
+
+        int j=0;
+        int k=0;
+        int l=0;
+        for(int i=0;i<list.size();i++){
+
+        Picture picture=list.get(i);
+        k=picture.getWidth();
+
+        picture.translate(j,0);
+
+
+        if(picture.getHeight()>k){
+        picture.draw();
+        j+=k;
+        l++;
+        }
+        }
+        System.out.println(l);
+
+        }
+        }
+
+
+        5번답
+
+
+        int k=0;
+
+        for(int i=0;i<list.size();i++){
+
+        Picture picture=list.get(i);
+        k=picture.getWidth();
+
+
+        if(picture.getHeight()>k){
+        picture.draw();
+
+        break;
+
+        }
+
+
+        6번답
+
+        ArrayList<Picture>matches=new ArrayList<>();
+
+        for(int i=0;i<list.size();i++){
+
+        Picture picture=list.get(i);
+
+
+        if(picture.getHeight()>picture.getWidth()){
+        matches.add(picture);
+        }
+        }
+
+        }
+        }
+
+
+
+
+
+
+
---
+++ src/Main_Pr.java
@@ -0,0 +1,52 @@
+public class Main_Pr {
+    public static void main(String[] args) {
+
+                char[] abcCode =
+
+                        {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*',
+
+                                '(', ')',
+
+                                '}', ';', ':', ',', '.', '/'};
+
+                                //0     1   2     3    4    5    6    7   8     9
+                char[] numCode = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+
+                String src = "abc123";
+
+                String result = "";
+                //문자열 arc 의 문자를 charAt()으로 하나씩 읽어서 변환 후 result 에 저장
+                for (int i = 0; i < src.length(); i++) {
+
+                    char a = src.charAt(i);
+
+                    a.
+
+
+
+                   // char[] temp = src.toCharArray();
+
+
+
+
+                  //char[] a = src.toCharArray();
+
+
+
+
+
+
+
+                }
+
+                System.out.println("src:" + src);
+
+                System.out.println("result:" + result);
+
+            }
+
+        }
+
+
+
+
